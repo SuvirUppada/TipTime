523851801b3aa62dd255d16e9a7264cb116b2e95
SuvirUppada <Suvir.Uppada2007@gmail.com>
2024-09-11 22:32:49 -0500

    Finished U2P3

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -48,6 +48,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Surface(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -65,11 +70,15 @@ fun TipTimeLayout() {
                     .padding(bottom = 16.dp, top = 40.dp)
                     .align(alignment = Alignment.Start)
             )
-            EditNumberField(modifier = Modifier
+            EditNumberField(
+                value =  amountInput,
+                onValueChange = {amountInput = it} ,
+                modifier = Modifier
                 .padding(bottom = 32.dp)
-                .fillMaxWidth())
+                .fillMaxWidth(),
+            )
             Text(
-                text = stringResource(R.string.tip_amount, "$0.00"),
+                text = stringResource(R.string.tip_amount, tip),
                 style = MaterialTheme.typography.displaySmall
             )
             Spacer(modifier = Modifier.height(150.dp))
@@ -79,15 +88,11 @@ fun TipTimeLayout() {
 
 
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
-    var amountInput by remember { mutableStateOf("")}
-
-    val amount = amountInput.toDoubleOrNull()?: 0.00
-    val tip = calculateTip(amount = amount)
+fun EditNumberField(value: String, onValueChange:(String) -> Unit, modifier: Modifier = Modifier){
 
     TextField(
-        value = amountInput,
-        onValueChange = {amountInput = it},
+        value = value,
+        onValueChange = onValueChange,
         label = {Text(stringResource(id = R.string.bill_amount))},
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
